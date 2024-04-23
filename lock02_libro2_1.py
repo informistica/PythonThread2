4efd702e447cf754739af4e033a9dc3d9754692a
informistica <mauro.spinarelli@gmail.com>
2024-04-23 10:23:08 +0200

    tolti commenti

--- lock02_libro2_1.py
+++ lock02_libro2_1.py
@@ -1,6 +1,9 @@
 import threading
 import time
-lock = threading.Lock()
+lock = threading.Lock() #utilizzata per gestire la mutua esclusione
+#Un oggetto Lock è un meccanismo di sincronizzazione 
+# per acquisire e rilasciare una risorsa in modo che solo un thread alla 
+# volta possa eseguire una sezione di codice critica.
 
 # Variabili condivise
 vet_dati = [None] * 5  # Inizializzazione di una lista vuota di 5 elementi
@@ -8,30 +11,25 @@ vet_dati = [None] * 5  # Inizializzazione di una lista vuota di 5 elementi
 vet_dati[0]=2
 vet_dati[1]=5
 vet_dati[2]=6
-ultimo = 2  # Indice dell'ultimo elemento inserito nel vettore
+ultimo = 2 
 print("vet_dati=",vet_dati)
 print("ultimo=",ultimo)
-# Funzione del produttore
+
 def produttore():
     global ultimo
-    lock.acquire()
+    lock.acquire() #acquisisce il blocco. Se il blocco è già acquisito da un altro thread, il thread corrente viene sospeso fino a quando il blocco non diventa disponibile
     ultimo += 1
     valore=10
     time.sleep(1)
-    # Inserisce il valore nella posizione libera trovata
     vet_dati[ultimo] = valore
-    # Stampa il vettore dopo l'inserimento
     print(f"Produttore: Inserito valore {valore} in posizione {ultimo} nel vettore: {vet_dati}")
-    lock.release()
+    lock.release() # rilascia il blocco precedentemente acquisito. Dopo il rilascio del blocco, altri thread possono acquisirlo e continuare l'esecuzione della sezione di codice critica protetta dalla lock
 
-# Funzione del consumatore
 def consumatore():
     global ultimo
     lock.acquire()
-    # Legge l'ultimo valore inserito e lo azzera
     valore = vet_dati[ultimo]
     vet_dati[ultimo] = 0
-    # Stampa il valore letto e il vettore dopo l'azzeramento
     print(f"Consumatore: Letto valore {valore} in posizione {ultimo} dal vettore: {vet_dati}")
     ultimo=ultimo-1
     time.sleep(1)  # Simula un'attesa prima di leggere un nuovo valore
